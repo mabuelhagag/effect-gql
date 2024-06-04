68196b5178537b644fa46794d871eb5cfdc086bf
Mohamad Abuelhagag <mhagag@uplandsoftware.com>
2024-06-04 23:27:42 +0300

    [Initialize Monorepo #1] Express server
    Signed-off-by: Mohamad Abuelhagag <mhagag@uplandsoftware.com>

--- example/scripts/dev.js
+++ example/scripts/dev.js
@@ -1,16 +1,51 @@
-import { fileURLToPath } from "url";
-import { createServer } from "vite";
+import { createRequestHandler } from "@remix-run/express";
+import compression from "compression";
+import express from "express";
+import morgan from "morgan";
 
-const __dirname = fileURLToPath(new URL("..", import.meta.url));
+const viteDevServer =
+  process.env.NODE_ENV === "production"
+    ? undefined
+    : await import("vite").then((vite) =>
+        vite.createServer({
+          server: { middlewareMode: true },
+        })
+      );
 
-(async () => {
-  const server = await createServer({
-    configFile: "vite.config.ts",
-    root: __dirname,
-    server: {
-      port: 3000,
-    },
-  });
-  await server.listen();
-  server.printUrls();
-})();
+const remixHandler = createRequestHandler({
+  build: viteDevServer
+    ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
+    : await import("./build/server/index.js"),
+});
+
+const app = express();
+
+app.use(compression());
+
+// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+app.disable("x-powered-by");
+
+// handle asset requests
+if (viteDevServer) {
+  app.use(viteDevServer.middlewares);
+} else {
+  // Vite fingerprints its assets so we can cache forever.
+  app.use(
+    "/assets",
+    express.static("build/client/assets", { immutable: true, maxAge: "1y" })
+  );
+}
+
+// Everything else (like favicon.ico) is cached for an hour. You may want to be
+// more aggressive with this caching.
+app.use(express.static("build/client", { maxAge: "1h" }));
+
+app.use(morgan("tiny"));
+
+// handle SSR requests
+app.all("*", remixHandler);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+  console.log(`Express server listening at http://localhost:${port}`)
+);
--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,4 +1,4 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import babel from "vite-plugin-babel";
 import tsconfigPaths from "vite-tsconfig-paths";
